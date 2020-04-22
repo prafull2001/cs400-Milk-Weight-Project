fb81f5396c1a8b942c2929b8cc8406c0fa0009b4
Shrey Shah <shreykshah@gmail.com>
2020-04-21 21:05:47 -0700

    finished base GUI

--- HelloFX/application/Main.java
+++ HelloFX/application/Main.java
@@ -4,8 +4,6 @@ import javafx.scene.control.*;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
-import java.io.FileInputStream;
 import java.util.List;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -19,6 +17,7 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.event.EventHandler;
 
 
 public class Main extends Application {
@@ -41,16 +40,20 @@ public class Main extends Application {
         VBox leftCol = new VBox();
         HBox leftRow1 = new HBox();
         Label queryLabel = new Label("QUERY TYPE: ");
-        String query_types[] = { "Farm", "Annual", "Monthly", "Date Range"}; 
+        String query_types[] = {"Year", "Month", "Date Range"}; 
         ComboBox query_type = new ComboBox(FXCollections.observableArrayList(query_types));
+        query_type.getSelectionModel().select(1);
         leftRow1.getChildren().addAll(queryLabel, query_type);
         HBox leftRow2 = new HBox();
-        Label inputLabel = new Label("INPUT DATA: ");
+        //Label inputLabel = new Label("INPUT DATA: ");
         TextField queryDataFarmID = new TextField("Farm ID"); 
-        TextField queryDataTime = new TextField("Time Range"); 
-        inputLabel.setStyle("-fx-padding: 4;");
+        TextField queryDataTime = new TextField("Month"); 
+        query_type.setOnAction((e) -> {
+        	queryDataTime.setText(query_type.getValue().toString());
+        });
+        //inputLabel.setStyle("-fx-padding: 4;");
         queryDataFarmID.setStyle("-fx-padding: 4;");
-        leftRow2.getChildren().addAll(inputLabel, queryDataFarmID, queryDataTime);
+        leftRow2.getChildren().addAll(/*inputLabel, */queryDataFarmID, queryDataTime);
         leftCol.getChildren().addAll(leftRow1, leftRow2);
         leftCol.setStyle("-fx-padding: 10;"+"-fx-border-width: 5;");
         
@@ -58,8 +61,12 @@ public class Main extends Application {
         VBox midCol = new VBox();
         VBox midLeftCol = new VBox();
         VBox midRightCol = new VBox();
-        RadioButton rb1 = new RadioButton("ADD DATA");
-        RadioButton rb2 = new RadioButton("REMOVE DATA");
+        final ToggleGroup group = new ToggleGroup();
+        RadioButton rb1 = new RadioButton("ADD");
+        RadioButton rb2 = new RadioButton("REMOVE");
+        rb1.setToggleGroup(group);
+        rb1.setSelected(true);
+        rb2.setToggleGroup(group);
         midLeftCol.setAlignment(Pos.BASELINE_RIGHT);
         midRightCol.setAlignment(Pos.BASELINE_RIGHT);
         midLeftCol.getChildren().add(rb1);
@@ -74,18 +81,26 @@ public class Main extends Application {
         //RIGHT PANE COMPONENTS
         VBox rightCol = new VBox();
         HBox rightRow1 = new HBox();
-        Label dataTypeLabel = new Label("DATA TYPE: ");
+        //Label dataTypeLabel = new Label("DATA TYPE: ");
         Label manageFile = new Label("MANAGE FILES");
         manageFile.setFont(new Font("Arial", 16));
         String data_types[] = { "File", "Entry"}; 
         ComboBox data_type = new ComboBox(FXCollections.observableArrayList(data_types));
-        rightRow1.getChildren().addAll(dataTypeLabel, data_type);
+        data_type.getSelectionModel().select(0);
+        rightRow1.getChildren().addAll(/*dataTypeLabel, */data_type);
         HBox rightRow2 = new HBox();
-        dataTypeLabel.setStyle("-fx-padding: 4;");
-        inputLabel.setStyle("-fx-padding: 4;");
-        TextField addData = new TextField("input data here..."); 
+        //dataTypeLabel.setStyle("-fx-padding: 4;");
+        //inputLabel.setStyle("-fx-padding: 4;");
+        TextField addData = new TextField("File Path"); 
+        data_type.setOnAction((e) -> {
+        	if (query_type.getValue().toString().contentEquals("File")) {
+        		addData.setText("File Path");
+        	} else {
+        		addData.setText("Farm ID, Date, Pounds");
+        	}
+        });
         addData.setStyle("-fx-padding: 4;");
-        rightRow2.getChildren().addAll(dataTypeLabel, addData);
+        rightRow2.getChildren().addAll(/*dataTypeLabel, */addData);
         rightCol.getChildren().addAll(rightRow1, rightRow2);
         rightCol.setStyle("-fx-padding: 10;"+"-fx-border-width: 5;");
         
