1103d13592db3f57fc185530581a511d0af7d979
shreykshah <50879398+shreykshah@users.noreply.github.com>
2020-04-30 19:17:38 -0700

    Sanitized inputs

--- HelloFX/application/DataManager.java
+++ HelloFX/application/DataManager.java
@@ -17,8 +17,11 @@ public class DataManager {
   
   public DataManager(String filePath) {
     inputFile = file.getPath();
-    
   }
+  
+  /*
+  TO DO : getDataSortedByField()
+  */
   
   private void readData() {
     try {
@@ -43,43 +46,89 @@ public class DataManager {
     }
   }
   
-  public String getMonthlyAverage() {
-    return cf.getMonthlyAverage();
+  public String getMonthlyAverage(String month) {
+    return cf.getMonthlyAverage(sanitizeMonthInput(month));
   }
   
-  public String getMonthlyMin() {
-    return cf.getMonthlyMin();
+  public String getMonthlyMin(String month) {
+    return cf.getMonthlyMin(sanitizeMonthInput(month));
   }
 
-  public String getMonthlyMax() {
-    return cf.getMonthlyMax();
+  public String getMonthlyMax(String month) {
+    return cf.getMonthlyMax(sanitizeMonthInput(month));
   }
   
-  public String getMonthlyAverageForFarm() {
-    return cf.getMonthlyAverageForFarm();
+  public String getMonthlyAverageForFarm(String month, String farm) {
+    return cf.getMonthlyAverageForFarm(sanitizeMonthInput(month), sanitizeFarmInput(farm));
   }
   
-  public String getMonthlyMinForFarm() {
-    return cf.getMonthlyMinForFarm();
+  public String getMonthlyMinForFarm(String month, String farm) {
+    return cf.getMonthlyMinForFarm(sanitizeMonthInput(month), sanitizeFarmInput(farm));
   }
   
-  public String getMonthlyMaxForFarm() {
-    return cf.getMonthlyMaxForFarm();
+  public String getMonthlyMaxForFarm(String month, String farm) {
+    return cf.getMonthlyMaxForFarm(sanitizeMonthInput(month), sanitizeFarmInput(farm));
   }
   
   public String getDataSortedByField() {
     return cf.getDataSortedByField();
   }
   
-  public String getAverageInDateRange() {
-    return cf.getAverageInDateRange();
+  public String getAverageInDateRange(String start, String end) {
+    return cf.getAverageInDateRange(sanitizeDateInput(start), sanitizeDateInput(end));
   }
   
-  public String getMinInDateRange() {
-    return cf.getMinInDateRange();
+  public String getMinInDateRange(String start, String end) {
+    return cf.getMinInDateRange(sanitizeDateInput(start), sanitizeDateInput(end);
   }
   
-  public String getMaxInDateRange() {
-    return cf.getMaxInDateRange();
+  public String getMaxInDateRange(String start, String end) {
+    return cf.getMaxInDateRange(sanitizeDateInput(start), sanitizeDateInput(end);
+  }
+  
+  private String sanitizeFarmInput(String farm) {
+    try {
+      try {
+        return ("Farm " + Integer.parseInt(farm).toString); //check if just number entered
+      } catch (NumberFormatException e) {
+        String[] farmCheck = farm.toLowerCase().split(" ");
+        if (farmCheck.length != 2) throw new IllegalArgumentException("invalid input");
+        try {
+          int ID = Integer.parseInt(farmCheck[1]);
+          if (!farmCheck[0].toLowerCase().equals("farm")) throw new IllegalArgumentException("invalid input");
+          return ("Farm " + ID);
+        } catch (NumberFormatException e) {
+          if (farmCheck.length != 2) throw new IllegalArgumentException("invalid input");
+        }
+      }
+    } catch (Exception e) {
+      throw new IllegalArgumentException("invalid input");
+    }
+  }
+  
+  private String sanitizeDateInput(String date) {
+    try {
+      String[] dateCheck = date.toLowerCase().split("-");
+      if (dateCheck.length != 3) throw new IllegalArgumentException("invalid input");
+      if (Integer.parseInt(dateCheck[0]) < 0) 
+        throw new IllegalArgumentException("only positive years");
+      if (Integer.parseInt(dateCheck[1]) > 12 || Integer.parseInt(farmCheck[1]) < 1)
+        throw new IllegalArgumentException("invalid input");
+      if (Integer.parseInt(dateCheck[2]) > 31 || Integer.parseInt(farmCheck[2]) < 1)
+        throw new IllegalArgumentException("invalid input");
+      return (dateCheck[0] + "-" + dateCheck[1] + "-" + dateCheck[2]);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("invalid input");
+    }
+  }
+  
+  private String sanitizeMonthInput(String date) {
+    try {
+      if (Integer.parseInt(date) > 12 || Integer.parseInt(date) < 1)
+        throw new IllegalArgumentException("invalid input");
+      return date;
+    } catch (Exception e) {
+      throw new IllegalArgumentException("invalid input");
+    }
   }
 }
