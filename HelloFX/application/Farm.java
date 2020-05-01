f22d75e460da2b94672462e22de8c706d68205d8
Dhruv Jain <Dhruv-J@users.noreply.github.com>
2020-04-30 20:56:43 -0700

    Update Farm.java

--- HelloFX/application/Farm.java
+++ HelloFX/application/Farm.java
@@ -76,4 +76,99 @@ public class Farm {
   public String getID() {
     return farmID;
   }
+  
+  public double getMonthAverage(int month) {
+    ArrayList<String> arr = milkWeights.get(month);
+    if(arr==null) {
+      return 0;
+    }
+    int total=0, count=0;
+    for(String s: arr) {
+      total+=Integer.parseInt(s.split(" ")[1]);
+      count+=1;
+    }
+    return Math.round((double)total/count);
+  }
+  
+  public int getMin(int month) {
+    ArrayList<String> arr = milkWeights.get(month);
+    if(arr==null) {
+      return Integer.MAX_VALUE;
+    }
+    int min = Integer.MAX_VALUE;
+    for(String s: arr) {
+      int cur = Integer.parseInt(s.split(" ")[1]);
+      if(cur<min) {
+        min = cur;
+      }
+    }
+    return min;
+  }
+  public int getMax(int month) {
+    ArrayList<String> arr = milkWeights.get(month);
+    if(arr==null) {
+      return 0;
+    }
+    int max = 0;
+    for(String s: arr) {
+      int cur = Integer.parseInt(s.split(" ")[1]);
+      if(cur>max) {
+        max = cur;
+      }
+    }
+    return max;
+  }
+  
+  public double getAverageInRange(String start, String end) {
+    int startMonth = Integer.parseInt(start.split("-")[1])-1;
+    int endMonth = Integer.parseInt(end.split("-")[1])-1;
+    int total=0, count=0;
+    for(int i=startMonth; i<=endMonth; i++) {
+      for(String s: milkWeights.get(i)) {
+        String date = s.split(" ")[0];
+        int cur = Integer.parseInt(s.split(" ")[1]);
+        if(dateCompare(date, start, end)) {
+          total += cur;
+          count += 1;
+        }
+      }
+    }
+    return Math.round((double)total/count);
+  }
+  
+  public int getMinInRange(String start, String end) {
+    int startMonth = Integer.parseInt(start.split("-")[1])-1;
+    int endMonth = Integer.parseInt(end.split("-")[1])-1;
+    int min = Integer.MAX_VALUE;
+    for(int i=startMonth; i<=endMonth; i++) {
+      for(String s: milkWeights.get(i)) {
+        String date = s.split(" ")[0];
+        int cur = Integer.parseInt(s.split(" ")[1]);
+        if(dateCompare(date, start, end)&&cur<min) {
+          min=cur;
+        }
+      }
+    }
+    return min;
+  }
+  
+  public int getMaxInRange(String start, String end) {
+    int startMonth = Integer.parseInt(start.split("-")[1])-1;
+    int endMonth = Integer.parseInt(end.split("-")[1])-1;
+    int max = 0;
+    for(int i=startMonth; i<=endMonth; i++) {
+      for(String s: milkWeights.get(i)) {
+        String date = s.split(" ")[0];
+        int cur = Integer.parseInt(s.split(" ")[1]);
+        if(dateCompare(date, start, end)&&cur>max) {
+          max=cur;
+        }
+      }
+    }
+    return max;
+  }
+  
+  private boolean dateCompare(String date1, String date2, String date3) {
+    return date1.compareTo(date2)>=0&&date1.compareTo(date3)<=0;
+  }
 }
