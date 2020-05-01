8f5e44ff45b76953e0729557222f1ec4b4f1add6
Dhruv Jain <Dhruv-J@users.noreply.github.com>
2020-04-30 18:32:24 -0700

    Update CheeseFactory.java

--- HelloFX/application/CheeseFactory.java
+++ HelloFX/application/CheeseFactory.java
@@ -2,12 +2,17 @@ package application;
 
 import java.util.Hashtable;
 
+/**
+ * 
+ * @author Dhruv Jain
+ * 
+ * This class 
+ *
+ */
 public class CheeseFactory {
-  private String name;
   Hashtable<String, Farm> milkDataFromFarms;
   
-  public CheeseFactory(String name) {
-    this.name = name;
+  public CheeseFactory() {
     milkDataFromFarms = new Hashtable<String, Farm>();
   }
   
@@ -18,6 +23,18 @@ public class CheeseFactory {
   public boolean insertData(String data) {
     boolean ret = true;
     String[] datas = data.split("\n");
+    for(String s: datas) {
+      if(ret&&!insertSingleData(s)) {
+        ret = false;
+      } else {
+        insertSingleData(s);
+      }
+    }
+    return ret;
+  }
+  
+  public boolean insertData(String[] datas) {
+    boolean ret = true;
     for(String s: datas) {
       if(ret&&!insertSingleData(s)) {
         ret = false;
@@ -41,27 +58,6 @@ public class CheeseFactory {
       return true;
     } else {
       return milkDataFromFarms.get(index).insertMilkForDate(data);
-    }
-  }
-  
-  /**
-   * @param oldData - data that is currently in the hash table
-   * @param newData - data to replace the current data in the hash table
-   * @return - true if data was edited successfully
-   */
-  public boolean editSingleData(String oldData, String newData) {
-    String[] oldDatas = oldData.split(",");
-    int index = oldDatas[1].hashCode();
-    oldData = oldDatas[0]+" "+oldDatas[2];
-    String[] newDatas = newData.split(",");
-    newData = newDatas[0]+" "+newDatas[2];
-    if(!oldDatas[0].equals(newDatas[0])) {
-      return false;
-    }
-    if(milkDataFromFarms.get(index)==null) {
-      return false;
-    } else {
-      return milkDataFromFarms.get(index).editMilkForDate(oldData, newData);
     }
   }
   
